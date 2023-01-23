1633b9483fd82134dd2e2c2eb510d435dc3332d0
Denis Ryzhankov <denryzhankov@gmail.com>
2023-01-23 19:03:24 +0300

    fix errors

--- guess_the_number.py
+++ guess_the_number.py
@@ -31,12 +31,15 @@ async def process_help_command(message: types.Message):
 
 # Этот хэндлер должен срабатывать на "Да", "Давай" или "Сыграем"
 async def process_yes_command(message: types.Message):
-    game['game_started'] = True
-    game['stat_game_total'] += 1
-    game['hidden_number'] = randint(0, 99)
-    game['count'] = 5
-    ANSWER = ("Я загадал число от 0 до 99, попробуй отгадать,"
-              f" у тебя {game['count']} попыток")
+    if game['game_started']:
+        ANSWER = 'Игра уже запущена'
+    else:
+        game['game_started'] = True
+        game['stat_game_total'] += 1
+        game['hidden_number'] = randint(0, 99)
+        game['count'] = 5
+        ANSWER = ("Я загадал число от 0 до 99, попробуй отгадать,"
+                  f" у тебя {game['count']} попыток")
     await message.answer(ANSWER)
 
 
@@ -58,32 +61,36 @@ async def process_cancel_command(message: types.Message):
 
 # Этот хэндлер должен срабатывать на число
 async def process_number_command(message: types.Message):
-    game['count'] -= 1
-    number = int(message['text'])
-    diff = game['hidden_number'] - number
+    if game['game_started']:
+        game['count'] -= 1
+        number = int(message['text'])
+        diff = game['hidden_number'] - number
 
-    if diff == 0:
-        game['game_started'] = False
-        game['stat_game_won'] += 1
-        ANSWER = (f"Ты угадал c {5-game['count']} попытки -"
-                  f" загаданное число {game['hidden_number']}")
-    elif game['count'] == 0:
-        game['game_started'] = False
-        ANSWER = ("У тебя не осталось попыток -"
-                  f" загаданное число {game['hidden_number']}")
-    elif diff < 0:
-        ANSWER = (f"Загаданное число меньше {number},"
-                  f" у тебя осталось {game['count']} попытки")
-    elif diff > 0:
-        ANSWER = (f"Загаданное число больше {number},"
-                  f" у тебя осталось {game['count']} попытки")
+        if diff == 0:
+            game['game_started'] = False
+            game['stat_game_won'] += 1
+            ANSWER = (f"Ты угадал c {5-game['count']} попытки -"
+                      f" загаданное число {game['hidden_number']}")
+        elif game['count'] == 0:
+            game['game_started'] = False
+            ANSWER = ("У тебя не осталось попыток -"
+                      f" загаданное число {game['hidden_number']}")
+        elif diff < 0:
+            ANSWER = (f"Загаданное число меньше {number},"
+                      f" у тебя осталось {game['count']} попытки")
+        elif diff > 0:
+            ANSWER = (f"Загаданное число больше {number},"
+                      f" у тебя осталось {game['count']} попытки")
+    else:
+        ANSWER = 'Для начала игры введите "Да", "Давай" или "Сыграем"'
     await message.answer(ANSWER)
 
 
 # Этот хэндлер будет срабатывать на любые другие текстовые сообщения
 async def send_echo(message: types.Message):
     if game['game_started']:
-        ANSWER = 'загадано число от 0 до 99'
+        ANSWER = ('Загадано число от 0 до 99,'
+                  f" осталось {game['count']} попытки")
     else:
         ANSWER = ('Сейчас бот умее только играть в "Угадай число"\n'
                   'Для начала игры введите "Да", "Давай" или "Сыграем"')
@@ -95,12 +102,12 @@ dp.register_message_handler(process_start_command, commands='start')
 dp.register_message_handler(process_help_command, commands='help')
 dp.register_message_handler(process_stat_command, commands='stat')
 dp.register_message_handler(process_cancel_command,
-                            Text(equals=['хватит', "довольно", "Надоело"]),
-                            commands='cancel')
-dp.register_message_handler(process_yes_command,
-                            Text(equals=["Да", "Давай", "Сыграем", "yes"]))
+                            Text(equals=['Хватит', "Надоело", "Стоп"]))  # ,
+# commands='cancel')
 dp.register_message_handler(process_number_command,
                             Text([str(x) for x in range(100)]))
+dp.register_message_handler(process_yes_command,
+                            Text(equals=["Да", "Давай", "Сыграем", "Yes"]))
 dp.register_message_handler(send_echo)
 
 
