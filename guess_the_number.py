ee60f0fd015638a05449856755cad898910e2e10
Denis Ryzhankov <denryzhankov@gmail.com>
2023-01-23 17:52:02 +0300

    add empty handlers

--- guess_the_number.py
+++ guess_the_number.py
@@ -1,4 +1,5 @@
 from aiogram import Bot, Dispatcher, executor, types
+from aiogram.dispatcher.filters import Text
 from settings import TG_TOKEN
 
 
@@ -9,17 +10,38 @@ dp: Dispatcher = Dispatcher(bot)
 
 # Этот хэндлер будет срабатывать на команду "/start"
 async def process_start_command(message: types.Message):
-    await message.answer('Привет!\nМеня зовут Эхо-бот!\nНапиши мне что-нибудь')
+    await message.answer('Привет!\nЭто тестовый бот для обучения')
 
 
 # Этот хэндлер будет срабатывать на команду "/help"
 async def process_help_command(message: types.Message):
-    await message.answer('Напиши мне что-нибудь и в ответ'
-                         ' я пришлю тебе твое сообщение')
+    await message.answer(
+        'Это тестовый бот для обучения.\n'
+        'Сейчас он должен научиться играть в игру "Угадай число"\n'
+        'Для начала игры введите "Да", "Давай" или "Сыграем"')
 
 
-# Этот хэндлер будет срабатывать на любые ваши текстовые сообщения,
-# кроме команд "/start" и "/help"
+# Этот хэндлер должен срабатывать на "Да", "Давай" или "Сыграем"
+async def process_yes_command(message: types.Message):
+    await message.answer('Это число')
+
+
+# Этот хэндлер должен срабатывать на "/stat"
+async def process_stat_command(message: types.Message):
+    await message.answer('Это число')
+
+
+    # Этот хэндлер должен срабатывать на "/cancel"
+async def process_cancel_command(message: types.Message):
+    await message.answer('Это число')
+
+
+# Этот хэндлер должен срабатывать на число
+async def process_number_command(message: types.Message):
+    await message.answer('Это число')
+
+
+# Этот хэндлер будет срабатывать на любые другие текстовые сообщения
 async def send_echo(message: types.Message):
     await message.reply(message.text)
 
@@ -27,6 +49,10 @@ async def send_echo(message: types.Message):
 # Регистрируем хэндлеры
 dp.register_message_handler(process_start_command, commands='start')
 dp.register_message_handler(process_help_command, commands='help')
+dp.register_message_handler(process_help_command, commands='stat')
+dp.register_message_handler(process_help_command, commands='cancel')
+dp.register_message_handler(process_number_command,
+                            Text([str(x) for x in range(100)]))
 dp.register_message_handler(send_echo)
 
 
